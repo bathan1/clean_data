832dce1a2254674357d6fd637e466cfb49baa03b
bathan <side_nathan@hotmail.com>
2023-08-23 23:31:09 -0400

    Clean up calculations csv

--- cleaner.py
+++ cleaner.py
@@ -6,9 +6,10 @@ clean_csv = './csvs/clean_sheet.csv'
 patients_df = pd.read_csv(clean_csv)
 # Strip text to just get ID number of patients
 patients_df['CleanID'] = patients_df['Cnmc_ID'].str.replace(r'CNMC_', '')
+print(patients_df.columns)
 # Change column names
 patients_df.columns = range(len(patients_df.columns))
-print(patients_df.head())
+print(patients_df.columns)
 
 # Import calculations csv file
 calculations_csv = './csvs/calculations.csv'
@@ -16,6 +17,7 @@ calculations_csv = './csvs/calculations.csv'
 calculations_df = pd.read_csv(calculations_csv)
 # Strip text to just get ID number of patients
 calculations_df['CleanID'] = calculations_df['Subject ID'].str.replace(r'CNMC_', '')
+print(calculations_df.columns)
 # Change column names
 calculations_df.columns = range(len(calculations_df.columns))
-print(calculations_df.head())
+print(calculations_df.columns)
