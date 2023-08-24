da2bfa40360a0ea640e2fe23717a456542b6480b
bathan <side_nathan@hotmail.com>
2023-08-24 01:46:00 -0400

    Filter out patients with no measurements

--- combiner.py
+++ combiner.py
@@ -15,6 +15,18 @@ for index_p, row_patients in patients_df.iterrows():
                     patients_df.at[index_p, col_num] = col_value
                     col_num += 1
 
+# Delete patients without measurements
+count_deleted_patients = 0
+file = open('no_measurements.txt', 'w')
+for index_p, row_patients in patients_df.iterrows():
+    if pd.isna(row_patients[22]):
+        count_deleted_patients += 1 # Increment count
+        file.write(str(row_patients[0]) + '\n') # Write out patient ID 
+        patients_df.drop(index_p, inplace=True) # Drop the patient
+# Write out total amount of patients whose measurements weren't included and close file
+file.write(str(count_deleted_patients))
+file.close()
+
 # Delete unnecessary columns
 cols_to_delete = [1, 2, 6, 7, 8, 9, 10, 11, 16, 20, 21, 22, 42]
 combined_cleaned = patients_df.drop(cols_to_delete, axis=1)
